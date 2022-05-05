f1ea09276d773db649fe42902e836f90257004d1
unknown <ashishlatwal2409@gmail.com>
2022-05-05 15:12:14 +0530

    one

---
+++ OOPs/Loops1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	while(n>0){
+	    if(n%2==0){
+	        
+	        cout<<"number is even"<<endl;
+			break;
+	    }
+	    else{
+	        cout<<"number is odd"<<endl;
+			break;
+	    }
+	}
+	return
+	 0;
+}
